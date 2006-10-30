57a4eee012a52fda89e01311dbd9fd2d5aa209fe
duclad <duclad@49388426-8c20-0410-8b80-f7dfd80817aa>
2006-10-30 18:35:07 +0000

    Rejected commit: Default

---
+++ senro-examples/senro-simple-example/src/test/java/org/senro/metadata/provider/HibernateMetadataProviderTests.java
@@ -0,0 +1,85 @@
+package org.senro.metadata.provider;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.senro.demo.Apple;
+import org.senro.metadata.MetadataProvider;
+import org.senro.metadata.exception.NoMetadataFoundException;
+import org.senro.metadata.util.MetadataManagerUtils;
+import org.senro.metadata.impl.MetadataClass;
+import org.senro.metadata.impl.SenroMetadataFactory;
+import org.senro.metadata.impl.SenroMetadataManager;
+import org.senro.metadata.provider.reflection.HibernateMetadataProvider;
+import org.senro.metadata.provider.reflection.impl.HibernateMetadataClassImpl;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.EntityMode;
+import org.hibernate.mapping.RootClass;
+import org.senro.utils.ClassUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+/**
+ * Author: Claudiu Dumitrescu
+ */
+public class HibernateMetadataProviderTests {
+
+    private SenroMetadataManager metadataManager = new SenroMetadataManager();
+    private HibernateMetadataProvider hibernateProvider;
+
+
+    private LocalSessionFactoryBean buildLocalSessionFactoryBean() throws Exception {
+         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+         sessionFactoryBean.setConfigLocation(new DefaultResourceLoader().getResource("classpath:hibernate.cfg.xml"));
+         sessionFactoryBean.setConfigurationClass(org.hibernate.cfg.AnnotationConfiguration.class);
+         PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+         propertiesFactoryBean.setLocation(new DefaultResourceLoader().getResource("classpath:hibernate.properties"));
+         sessionFactoryBean.setHibernateProperties((Properties) propertiesFactoryBean.getObject());
+         sessionFactoryBean.afterPropertiesSet();
+         return sessionFactoryBean;
+     }
+
+    public Set getAllTypes(LocalSessionFactoryBean sessionFactoryBean) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Set allTypes = new HashSet();
+        for( Iterator iter = sessionFactoryBean.getConfiguration().getClassMappings(); iter.hasNext(); )
+        {
+            Object entry = iter.next();
+            allTypes.add( Class.forName((String) PropertyUtils.getProperty(entry,"className")) );
+        }
+        return allTypes;
+    }
+
+    @Before
+    public void init() throws Exception {
+        LocalSessionFactoryBean sessionFactoryBean = buildLocalSessionFactoryBean();
+        hibernateProvider = new HibernateMetadataProvider();
+        hibernateProvider.setSessionFactory(sessionFactoryBean);
+        SenroMetadataFactory metadataFactory = new SenroMetadataFactory();
+        metadataFactory.setMetadataProviders(Arrays.asList(new MetadataProvider[]{hibernateProvider}));
+        metadataManager.setMetadataFactory(metadataFactory);
+        metadataManager.setTypes(getAllTypes(sessionFactoryBean));
+        metadataManager.afterPropertiesSet();
+    }
+
+
+    @Test
+    public void getClassClass() {
+        assertEquals(HibernateMetadataClassImpl.class, hibernateProvider.getClassClass());
+    }
+
+    @Test
+    public void getClassMetadata() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, NoMetadataFoundException {
+        MetadataClass metadataClass = (MetadataClass) metadataManager.getMetadata(MetadataManagerUtils.getUniqueIdentifier(Apple.class));
+        Field identifierField = ClassUtils.getField(Apple.class, "id");
+        assertEquals(identifierField, PropertyUtils.getProperty(metadataClass, "identifierField"));
+    }
+}
+
+
+
