487f67de5c8ecad0edcfae8ec6e0da9beea6c905
duclad <duclad@49388426-8c20-0410-8b80-f7dfd80817aa>
2006-11-01 20:31:50 +0000

    Failed commit: Default

---
+++ senro-core/src/main/java/org/senro/component/ButtonPanel.java
@@ -0,0 +1,29 @@
+package org.senro.component;
+
+import wicket.markup.html.panel.Panel;
+import wicket.markup.html.form.Button;
+import wicket.model.Model;
+import org.senro.metadata.exception.NoMetadataFoundException;
+
+/**
+ * Author: Claudiu Dumitrescu
+ */
+public abstract class ButtonPanel extends Panel {
+
+    public ButtonPanel(String id) {
+        super("buttonPanel");
+        Button button = new Button("button", new Model(id)) {
+            protected void onSubmit() {
+                try {
+                    onClick();
+                } catch (NoMetadataFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        add(button);
+    }
+
+    public abstract void onClick() throws NoMetadataFoundException;
+
+}
---
+++ senro-core/src/main/java/org/senro/component/TextFieldPanel.java
@@ -0,0 +1,28 @@
+package org.senro.component;
+
+import wicket.markup.ComponentTag;
+import wicket.markup.html.form.TextField;
+import wicket.markup.html.panel.Panel;
+import wicket.model.PropertyModel;
+
+/**
+ * Author: Claudiu Dumitrescu
+ */
+public class TextFieldPanel extends Panel {
+
+    public TextFieldPanel(String id, Object entity) {
+        super("valuePanel");
+//        FormComponentFeedbackBorder formComponentFeedbackBorder = new FormComponentFeedbackBorder("propertyFeedback");
+        TextField child = new TextField("propertyValue", new PropertyModel(entity, id)) {
+            protected void onComponentTag(final ComponentTag tag) {
+                super.onComponentTag(tag);
+                tag.put("size", 10);
+            }
+        };
+
+        add(child);
+//        add(formComponentFeedbackBorder);
+
+    }
+
+}
