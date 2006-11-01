029db910ea082dfbf8bb3606fbe002024e30ea63
duclad <duclad@49388426-8c20-0410-8b80-f7dfd80817aa>
2006-11-01 20:31:50 +0000

    Failed commit: Default

---
+++ trunk/senro-core/src/main/java/org/senro/component/EditForm.java
@@ -0,0 +1,65 @@
+package org.senro.component;
+
+import org.senro.utils.ClassUtils;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.form.Form;
+import wicket.model.CompoundPropertyModel;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Claudiu Dumitrescu
+ */
+public class EditForm extends Form {
+
+
+    public EditForm(String id) {
+        super(id);
+    }
+
+
+    /**
+     * Constructor
+     *
+     * @param id     The name of this component
+     * @param entity Entity to be edited
+     */
+    public EditForm(final String id, final Object entity) {
+        // Construct formFields with no validation listener
+        super(id, new CompoundPropertyModel(entity));
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(entity.getClass());
+            List lppList = new ArrayList();
+            LabelPanelPair lpp = null;
+            Field field = null;
+            for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
+                try {
+                    field = ClassUtils.getField(entity.getClass(), descriptor.getName());
+                } catch (RuntimeException e) {
+                    field = null;
+                }
+                if (field != null) {
+                    lpp = new LabelPanelPair();
+                    lpp.setLabel(new Label("propertyLabel", descriptor.getName()));
+                    lpp.setPanel(new TextFieldPanel(descriptor.getName(), entity));
+                    lppList.add(lpp);
+                }
+            }
+
+            add(new FieldsListView("formFields", lppList));
+
+
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
---
+++ trunk/senro-core/src/main/java/org/senro/component/PageLinkPanel.java
@@ -0,0 +1,20 @@
+package org.senro.component;
+
+import wicket.markup.html.link.IPageLink;
+import wicket.markup.html.link.PageLink;
+import wicket.markup.html.panel.Panel;
+import wicket.markup.html.basic.Label;
+
+/**
+ * Author: Claudiu Dumitrescu
+ */
+public class PageLinkPanel extends Panel {
+
+
+    public PageLinkPanel(String linkId, IPageLink iPageLink) {
+        super("linkPanel");
+        PageLink pageLink = new PageLink("link", iPageLink);
+        pageLink.add(new Label("link",linkId));
+        add(pageLink);
+    }
+}
