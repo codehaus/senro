1e27d8a9b4dc07949f4777e6d52f9aa2af004bcc
duclad <duclad@49388426-8c20-0410-8b80-f7dfd80817aa>
2006-11-05 18:25:03 +0000

    simple metadata mechanism

--- trunk/senro-core/src/test/java/org/senro/metadata/util/MetadataAccessorTest.java
+++ trunk/senro-core/src/test/java/org/senro/metadata/util/MetadataAccessorTest.java
@@ -1,7 +1,37 @@
 package org.senro.metadata.util;
 
+import org.senro.metadata.MetadataProvider;
+import org.senro.metadata.Metadata;
+import org.senro.metadata.impl.simple.SimpleMetadataFactory;
+import org.senro.metadata.provider.reflection.ReflectionMetadataProvider;
+import org.senro.demo.good.Apple;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+
 /**
  * Created by <a href="mailto:claudiu.dumitrescu@gmail.com">Claudiu Dumitrescu</a>
  */
-public class MetadataAccessorTest {
+public class MetadataAccessorTest extends AbstractDependencyInjectionSpringContextTests {
+
+    private SimpleMetadataFactory metadataFactory;
+
+    protected String[] getConfigLocations() {
+        return new String[0];
+    }
+
+    public void onSetUp() throws Exception {
+        metadataFactory = new SimpleMetadataFactory();
+        ArrayList<MetadataProvider> list = new ArrayList<MetadataProvider>();
+        list.add(new ReflectionMetadataProvider());
+        metadataFactory.setMetadataProviders(list);
+    }
+
+
+    public void testReadMetadataInfo() throws Exception{
+        Metadata metadata = metadataFactory.createClass(Apple.class);
+        String isChild = MetadataAccessor.readMetadataInfo(metadata,  "child");
+        Assert.assertEquals(isChild,"false");
+    }
 }
