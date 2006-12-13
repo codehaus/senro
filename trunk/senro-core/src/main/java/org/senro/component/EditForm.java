bdc93f1b1c5d5bd3f12e33974d5aaf2fdcef3f12
duclad <duclad@49388426-8c20-0410-8b80-f7dfd80817aa>
2006-12-13 14:49:56 +0000

    folder with childs added for parent entities

--- trunk/senro-core/src/main/java/org/senro/component/EditForm.java
+++ trunk/senro-core/src/main/java/org/senro/component/EditForm.java
@@ -5,6 +5,9 @@ import org.senro.metadata.exception.NoMetadataFoundException;
 import org.senro.servlet.SenroApplication;
 import wicket.markup.html.form.Form;
 import wicket.model.CompoundPropertyModel;
+import wicket.Component;
+import wicket.extensions.markup.html.tabs.AbstractTab;
+import wicket.extensions.ajax.markup.html.tabs.AjaxTabbedPanel;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
@@ -32,18 +35,28 @@ public class EditForm extends Form implements MetadataAwareComponent {
         // Construct formFields with no validation listener
         super(id, new CompoundPropertyModel(entity));
         List lppList = new ArrayList();
+        List tabsList = new ArrayList();
         Metadata metadataClass = getMetadata(entity.getClass());
         for (Method property : metadataClass.getProperties()) {
             //todo Claudiu: check metadata against drools here
             try {
-                lppList.add(ComponentFactory.createFormComponent(property, entity));
+                Object formComponent = ComponentFactory.createFormComponent(property, entity);
+                if (formComponent instanceof LabelPanelPair) {
+                    lppList.add(formComponent);
+                } else if (formComponent instanceof AbstractTab) {
+                    tabsList.add(formComponent);
+                }
             } catch (NoMetadataFoundException e) {
                 e.printStackTrace();
             }
         }
 
         add(new FieldsListView("formFields", lppList));
-
+        if (!tabsList.isEmpty()) {
+            add(new AjaxTabbedPanel("tabs", tabsList));
+        }else{
+            add(new EmptyPanel("tabs"));
+        }
 
     }
 
--- trunk/senro-core/src/main/java/org/senro/metadata/provider/annotation/HibernateMetadataProperty.java
+++ trunk/senro-core/src/main/java/org/senro/metadata/provider/annotation/HibernateMetadataProperty.java
@@ -1,15 +1,22 @@
 package org.senro.metadata.provider.annotation;
 
 /**
- * Created by IntelliJ IDEA.
  * User: briantopping
  * Date: Nov 3, 2006
  * Time: 2:06:24 PM
- * To change this template use File | Settings | File Templates.
+ * Represents the metadata obtained from hibernate for a entity property
  */
 public interface HibernateMetadataProperty {
 
     boolean isManyToOne();
 
     void setManyToOne(boolean boolVal);
+
+    boolean isOneToMany();
+
+    void setOneToMany(boolean boolVal);
+
+    public void setTargetEntity(Class clazz);
+
+    public Class getTargetEntity();
 }
