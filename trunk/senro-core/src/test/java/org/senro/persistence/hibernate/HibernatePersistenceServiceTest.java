b81e1519354a06fb62035a1296677fcf4a7d79c0
topping <topping@49388426-8c20-0410-8b80-f7dfd80817aa>
2007-02-21 01:17:21 +0000

    Supplement Persistence manager to support record ranges, fix tests to actually read from HSQL database.

--- trunk/senro-core/src/test/java/org/senro/persistence/hibernate/HibernatePersistenceServiceTest.java
+++ trunk/senro-core/src/test/java/org/senro/persistence/hibernate/HibernatePersistenceServiceTest.java
@@ -1,36 +1,56 @@
 package org.senro.persistence.hibernate;
 
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
-import org.senro.persistence.PersistenceService;
+import org.hibernate.criterion.DetachedCriteria;
 import org.senro.demo.good.Apple;
+import org.senro.persistence.PersistenceService;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
-
-import wicket.extensions.markup.html.repeater.util.SortParam;
 
 /**
  * Author: Claudiu Dumitrescu
  */
 public class HibernatePersistenceServiceTest extends AbstractDependencyInjectionSpringContextTests {
     PersistenceService persistenceService;
+    DataSource dataSource;
 
+    protected void onSetUp() throws Exception {
+        JdbcTemplate template = new JdbcTemplate(dataSource);
+        String sql = new String(FileCopyUtils.copyToByteArray(getClass().getResourceAsStream("/testData.sql")));
+        template.execute(sql);
+    }
 
     public void setPersistenceService(PersistenceService persistenceService) {
         this.persistenceService = persistenceService;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     protected String[] getConfigLocations() {
         return new String[]{"classpath:senroMetadataManagerTestContext.xml"};
     }
 
-    public void testGetAllInstancesWithoutSorting(){
+    public void testGetAllInstancesWithoutSorting() {
         List appleList = persistenceService.getAllInstances(Apple.class);
         assertNotNull(appleList);
-
+        assertEquals(appleList.size(), 3);
     }
-    public void testGetAllInstancesWithSorting(){
-        List appleList = persistenceService.getAllInstances(Apple.class,new SortParam("id", true));
+
+    public void testCriteriaGet() {
+        DetachedCriteria query = DetachedCriteria.forClass(Apple.class);
+        List appleList = persistenceService.getAllInstances(query);
         assertNotNull(appleList);
-
+        assertEquals(appleList.size(), 3);
     }
+//   Why does core have a dependency on Wicket now? :)   
+//    public void testGetAllInstancesWithSorting(){
+//        List appleList = persistenceService.getAllInstances(Apple.class,new SortParam("id", true));
+//        assertNotNull(appleList);
+//
+//    }
 }
---
+++ trunk/senro-core/src/test/resources/testData.sql
@@ -0,0 +1,4 @@
+delete from fruit;
+insert into fruit (DTYPE, color) values ('Apple', 'red');
+insert into fruit (DTYPE, color) values ('Apple', 'yellow');
+insert into fruit (DTYPE, color) values ('Apple', 'green');
